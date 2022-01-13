f3f3eff5fd8966dedfa41e9b26a66721be6f5c56
Mayank17M <mamgain.mayank01@gmail.com>
2022-01-13 10:37:42 +0530

    Generate and Store keypairs

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const {
     LAMPORTS_PER_SOL,
     Transaction,
     Account,
-  } = require("@solana/web3.js");
+} = require("@solana/web3.js");
 
-  
+const newPair = new Keypair(); //New wallet keypair
+console.log(newPair);
+
+const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+const secretKey = newPair._keypair.secretKey;
