6b1edc5ea07cdde393f2a4b978b9530a8338c8fd
Mayank17M <mamgain.mayank01@gmail.com>
2022-01-13 10:57:28 +0530

    Get wallet balance

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ console.log(newPair);
 
 const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
 const secretKey = newPair._keypair.secretKey;
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const myWallet =  Keypair.fromSecretKey(secretKey);
+    const walletBalance = await connection.getBalance(
+      new PublicKey(myWallet.publicKey)
+    );
+    console.log(`=> For wallet address ${publicKey}`);
+    console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+  } catch (err) {
+    console.log(err);
+  }
+};
